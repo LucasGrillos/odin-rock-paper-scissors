b240ebd50f7696828a5c575568f0d9f7edc32850
Lucas Grillos <LucasGrillos@gmail.com>
2022-06-25 18:41:15 -0700

    Add half of gamestate

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
+let startGameButton = document.querySelector('#gamestart-button');
+let rpsSelectButtons = document.querySelectorAll('.rps-select-button');
+let gameContent = document.querySelector(".game-content");
+let playerHand = document.querySelector('.player-rps');
+let computerHand = document.querySelector('.computer-rps');
+
+let textToEmoji = {
+    'rock': '✊',
+    'paper': '✋',
+    'scissors': '✌️'
+};
+
+let emojiToText = {
+    '✊': 'rock',
+    '✋': 'paper',
+    '✌️': 'scissors'
+}
+
+window.addEventListener('load', function() {
+    startGameButton.addEventListener('click', startGame);
+}, false);
+
+const startGame = () => {
+    startGameButton.removeEventListener('click', startGame);
+    startGameButton.style.display = "none";
+    gameContent.classList.toggle("message");
+    rpsSelectButtons.forEach( button => {
+        button.style.display = 'block';
+        button.addEventListener('click', playDropAnimation);
+    });
+}
+
+const playDropAnimation = (event) => {
+    rpsSelectButtons.forEach( button => {
+        button.style.display = 'none';
+        button.removeEventListener('click', playDropAnimation);
+    })
+    
+    playerHand.style.display = "block";
+    computerHand.style.display = "block";
+    playerHand.textContent = textToEmoji[event.target.dataset.symbol];
+    computerHand.textContent = textToEmoji[computerPlay()];
+    playerHand.style.animation = "hand-animation .3s";
+    computerHand.style.animation = "hand-animation .3s";
+    playerHand.addEventListener('animationend', playShakeAnimation);
+    //console.log(event.target.dataset.symbol);
+}
+
+playRound = (event) => {
+    let determineWinState = {
+        "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
+        "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
+        "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
+    }
+
+    let playerSelection = emojiToText[playerHand.textContent];
+    let computerSelection = emojiToText[computerHand.textContent];
+    let determine = determineWinState[playerSelection][computerSelection]
+}
+
 const computerPlay = () => {
     let options = ["rock", "paper", "scissors"]
     return options[Math.floor(Math.random() * 3)];
 }
 
-const playRound = (playerSelection, computerSelection) => {
+const playRoundOTHER = (playerSelection, computerSelection) => {
     let determineWinState = {
         "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
         "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
