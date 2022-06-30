3602003b6cfa64f7976a1ace18487e44047a67d4
Lucas Grillos <LucasGrillos@gmail.com>
2022-06-29 23:51:44 -0700

    Complete game loop, restarting at five wins
    Need refactoring

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let body = document.querySelector("body");
 let startGameButton = document.querySelector('#gamestart-button');
 let rpsSelectButtons = document.querySelectorAll('.rps-select-button');
 let gameContent = document.querySelector(".game-content");
@@ -6,6 +7,10 @@ let computerHand = document.querySelector('.computer-rps');
 
 let winH2 = document.querySelector('#win-msg');
 let describeH2 = document.querySelector('#desc-message');
+let clickH2 = document.querySelector('#click-message');
+
+let playerScore = document.querySelector('#player-score');
+let computerScore = document.querySelector('#computer-score');
 
 let textToEmoji = {
     'rock': '✊',
@@ -20,13 +25,40 @@ let emojiToText = {
 };
 
 window.addEventListener('load', function() {
-    startGameButton.addEventListener('click', startGame);
+    addStartGameButton()
 }, false);
 
-const startGame = () => {
+const addStartGameButton = () => {
+    winH2.style.display = "none";
+    describeH2.style.display = "none";
+    clickH2.style.display = "none";
+    playerHand.style.display = "none";
+    computerHand.style.display = "none";
+    playerScore.textContent = "0";
+    computerScore.textContent = "0";
+    body.removeEventListener('click', addStartGameButton);
     
+    startGameButton.style.display = "block";
+    startGameButton.addEventListener('click', startGame);
+}
+
+const startGame = () => {
     startGameButton.removeEventListener('click', startGame);
     startGameButton.style.display = "none";
+    addGameButtons();
+}
+
+const repeatGame = () => {
+    winH2.style.display = "none";
+    describeH2.style.display = "none";
+    clickH2.style.display = "none";
+    playerHand.style.display = "none";
+    computerHand.style.display = "none";
+    body.removeEventListener('click', repeatGame);
+    addGameButtons();
+}
+
+const addGameButtons = () => {
     gameContent.classList.toggle("message");
     rpsSelectButtons.forEach( button => {
         button.style.display = 'block';
@@ -35,6 +67,7 @@ const startGame = () => {
 }
 
 const playDropAnimation = (event) => {
+
     rpsSelectButtons.forEach( button => {
         button.style.display = 'none';
         button.removeEventListener('click', playDropAnimation);
@@ -56,26 +89,55 @@ const playRound = (event) => {
         "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
         "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
     }
-    
+    playerHand.removeEventListener('animationend', playRound);
     let playerSelection = emojiToText[playerHand.textContent];
     let computerSelection = emojiToText[computerHand.textContent];
-    let determine = determineWinState[playerSelection][computerSelection]
-
-    console.log(playerSelection)
+    let results = determineWinState[playerSelection][computerSelection]
 
     gameContent.classList.toggle("message");
 
-    winH2.textContent = `You ${determine}!`
-    describeH2.textContent = `${playerSelection} ${determine}s against ${computerSelection}`
+    winH2.textContent = `You ${results}!`
+    describeH2.textContent = `${playerSelection} ${results}s against ${computerSelection}`
     winH2.style.display = 'block';
     describeH2.style.display = 'block';
-    incrementPlayerScore();
+    clickH2.style.display = 'block';
+
+    checkTie(results);
+}
+
+//toggleScores()
+
+const checkTie = (results) => {
+    const TIE = 'tie';
+    results == TIE ? clickAnywhereToContinue() : checkWin(results);
+}
+
+const checkWin = (results) => {
+    const WIN = 'win';
+    results == WIN ? incrementPlayerScore() : incrementComputerScore();
+    if(parseInt(playerScore.textContent) >= 5 || parseInt(computerScore.textContent) >= 5) {
+        let winOrLose = parseInt(playerScore.textContent) >= 5 ? "win" : "lose";
+        gameOver(winOrLose)
+    } 
+    else {
+        clickAnywhereToContinue('repeat');   
+    } 
+}
+
+const clickAnywhereToContinue = (restartOrRepeat) => {
+    restartOrRepeat == 'restart' ? body.addEventListener('click', addStartGameButton) : body.addEventListener('click', repeatGame);
 }
 
 const incrementPlayerScore = () => {
-    let playerScore = parseInt(document.querySelector('#player-score').textContent);
-    playerScore++;
-    document.querySelector('#player-score').textContent = playerScore;
+    let score = parseInt(playerScore.textContent);
+    score+=1;
+    playerScore.textContent = score;
+}
+
+const incrementComputerScore = () => {
+    let score = parseInt(computerScore.textContent);
+    score+=1;
+    computerScore.textContent = score; 
 }
 
 const computerPlay = () => {
@@ -83,23 +145,8 @@ const computerPlay = () => {
     return options[Math.floor(Math.random() * 3)];
 }
 
-const playRoundOTHER = (playerSelection, computerSelection) => {
-    let determineWinState = {
-        "rock": {"rock": "tie", "paper": "lose", "scissors": "win"},
-        "paper": {"rock": "win", "paper": "tie", "scissors": "lose"},
-        "scissors": {"rock": "lose", "paper": "win", "scissors": "tie"}
-    }
-
-    let determine = determineWinState[playerSelection][computerSelection]
-    console.log(`You ${determine}! ${playerSelection} ${determine}s against ${computerSelection}`)
-    return determine
+const gameOver = (winOrLose) => {
+    winH2.textContent = `YOU ${winOrLose}!`
+    describeH2.textContent = `${parseInt(playerScore.textContent)} games to ${parseInt(computerScore.textContent)} games`
+    clickAnywhereToContinue('restart')
 }
-
-let playerWins = 0
-let computerWins = 0
-
-console.log(playerWins==computerWins 
-    ? `You tie ${playerWins} games to ${computerWins}`
-    : (playerWins > computerWins 
-    ? `You win ${playerWins} games to ${computerWins}`
-    : `You lose ${playerWins} games to ${computerWins}`))
